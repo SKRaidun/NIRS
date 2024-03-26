fe713c8d76cbf91fb9d2638eb04b4a58dc5b8a6d
starastsinvi <starastsinvi@ya.ru>
2024-03-26 14:49:58 +0300

    2

--- read_file_txt.py
+++ read_file_txt.py
@@ -1,12 +1,24 @@
 from pathlib import Path
 from fileinput import filename
 
-path_file = Path('.')
+# функция на парсинг списка файлов формата .txt
+# в котором содержатся наши данные, возвращаем список файлов
 
-def read_sort_file():
+def read_sort_file_txt():
+    list_file = list(sorted(Path('.').glob('*.txt')))
+    return list_file
 
-    return 0
-#print(list(path_file.glob('**/*.txt')))
+print(len(read_sort_file_txt()))
+
+def get_name_datafiles():
+    i = 0
+    while i < len(read_sort_file_txt()):
+        list_name_file = read_sort_file_txt().split('_')
+        i += 1
+    return list_name_file
+
+#print(get_name_datafiles())
+
 
 # for file in files:
 #     fname = os.path.basename(file)
