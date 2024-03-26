8ea4130661648a24a7ea9c33aca1c2c402e6bc03
starastsinvi <starastsinvi@ya.ru>
2024-03-26 13:53:19 +0300

    отдельно хочу сделать блок на считывание данных. что бы можно было просто закинуть текстовый файл с данными и тут же потом реализовать ввод данных для расчета дифф сечений

---
+++ read_file_txt.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from fileinput import filename
+
+path_file = Path('.')
+
+def read_sort_file():
+
+    return 0
+#print(list(path_file.glob('**/*.txt')))
+
+# for file in files:
+#     fname = os.path.basename(file)
+#     dict_[fname] = (pd.read_csv(file, header=0, dtype=str, encoding='cp1252')
+#                       .fillna(''))
