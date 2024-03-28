ccb6b389c3d1b5953261d5c35c36c9498d6527c8
starastsinvi <starastsinvi@ya.ru>
2024-03-28 15:40:11 +0300

    5) сделан парсинг названия файлов в виде выходного словаря

--- read_file_txt.py
+++ read_file_txt.py
@@ -1,4 +1,6 @@
 import glob
+
+
 # from pathlib import Path
 # from fileinput import filename
 # import os
@@ -6,39 +8,21 @@ import glob
 # функция на парсинг списка файлов формата .txt
 # в котором содержатся наши данные, возвращаем список файлов
 
-# def read_sort_file_txt():
-#     list_file = list(sorted(Path('.').glob('*.txt')))
-#     return list_file
-
-# извлекаем название из файлов данных
-# def get_name_datafiles():
-#     i = 0
-#     while i < len(list_file_str):
-#         list_name_file = list_file_str[i].split('_')
-#         i += 1
-#     return list_name_file
-
-# def get_name_datafiles():
-#     i = 0
-#     list_name_file = list_file_str[i].split('_')
-#     return list_name_file
-
-
 def read_sort_file_txt():
-    #way_data = r'sorted_data_txt'
-    #os.chdir(way_data)
     list_file_txt = glob.glob('./sorted_data_txt/*.txt')
     return list_file_txt
 
-def get_name_datafiles():
-    id_file = 0
+
+# создаем словарь с параметрами рабочих файлов.
+def dict_datafiles():
     list_name_file = []
     list_id_file = []
     list_run = []
     list_type_detector = []
     list_contur = []
     list_degree = []
-    # dict_list_file = {}
+    dict_list_file = {}
+
     for id_file in range(len(read_sort_file_txt())):
         list_name_file.append(read_sort_file_txt()[id_file].split('_'))
         list_id_file.append(id_file)
@@ -47,16 +31,13 @@ def get_name_datafiles():
         list_contur.append(list_name_file[id_file][4])
         list_degree.append(list_name_file[id_file][5].replace('.txt', ''))
 
-    return list_id_file, list_run, list_degree, list_type_detector, list_contur
+    dict_list_file['id_file'] = list_id_file
+    dict_list_file['run'] = list_run
+    dict_list_file['type_detector'] = list_type_detector
+    dict_list_file['contur'] = list_contur
+    dict_list_file['degree'] = list_degree
+
+    return dict_list_file
 
 
-print(get_name_datafiles())
-# while i < len(read_sort_file_txt()):
-#     print(get_name_datafiles())
-#     i += 1
-
-
-# for file in files:
-#     fname = os.path.basename(file)
-#     dict_[fname] = (pd.read_csv(file, header=0, dtype=str, encoding='cp1252')
-#                       .fillna(''))
+print(dict_datafiles())
